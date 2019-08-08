ee9f8eb6d6909d1f841d034032926d1950e1f06d
RemiIO <i@linux.dog>
2019-08-08 20:10:50 +0800

    Add logic of upload.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { Plugin } = require('@uppy/core')
 const cuid = require('cuid')
+const cos = require('cos-js-sdk-v5')
 class CosUppy extends Plugin {
     constructor(uppy, opts) {
         super(uppy, opts)
@@ -69,13 +70,84 @@ class CosUppy extends Plugin {
         })
     }
 
+    // 获取签名
+    getAuthorization(options, callback) {
+        var url = this.stsUrl
+        //var url = '../server/sts.php';
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.onreadystatechange = function (e) {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    var credentials;
+                    try {
+                        credentials = (new Function('return ' + xhr.responseText))().credentials;
+                    } catch (e) { }
+                    if (credentials) {
+                        callback(null, {
+                            XCosSecurityToken: credentials.sessionToken,
+                            Authorization: CosAuth({
+                                SecretId: credentials.tmpSecretId,
+                                SecretKey: credentials.tmpSecretKey,
+                                Method: options.Method,
+                                Pathname: options.Pathname,
+                            })
+                        });
+                    } else {
+                        console.error(xhr.responseText);
+                        callback('获取签名出错');
+                    }
+                } else {
+                    callback('获取签名出错');
+                }
+            }
+        };
+        xhr.send();
+    };
+
+    uploadFile(file, callback) {
+        var Key = 'dir/' + file.name; // 这里指定上传目录和文件名
+        this.getAuthorization({ Method: 'POST', Pathname: '/' }, function (err, info) {
+            var fd = new FormData();
+            fd.append('key', Key);
+            fd.append('Signature', info.Authorization);
+            fd.append('Content-Type', '');
+            info.XCosSecurityToken && fd.append('x-cos-security-token', info.XCosSecurityToken);
+            fd.append('file', file);
+            var url = prefix;
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', url, true);
+            xhr.upload.onprogress = function (e) {
+                console.log('上传进度 ' + (Math.round(e.loaded / e.total * 10000) / 100) + '%');
+            };
+            xhr.onload = function () {
+                if (Math.floor(xhr.status / 100) === 2) {
+                    var ETag = xhr.getResponseHeader('etag');
+                    callback(null, { url: prefix + camSafeUrlEncode(Key).replace(/%2F/g, '/'), ETag: ETag });
+                } else {
+                    callback('文件 ' + Key + ' 上传失败，状态码：' + xhr.status);
+                }
+            };
+            xhr.onerror = function () {
+                callback('文件 ' + Key + ' 上传失败，请检查是否没配置 CORS 跨域规则');
+            };
+            xhr.send(fd);
+        });
+    };
+
+
     authorizationAndUpdate(file, current, total) {
         const opts = this.getOptions(file)
-        return new Promise((resolve, reject) => {
-            
 
-            const xhr = new XMLHttpRequest()
-            const id = cuid()
+        return new Promise((resolve, reject) => {
+            file && this.uploadFile(file, function (err, data) {
+                if (err) {
+                    reject()
+                }
+                console.log(err || data);
+                document.getElementById('msg').innerText = err ? err : ('上传成功，ETag=' + data.ETag);
+                resolve()
+            });
         })
     }
 
